2244e75840ec38f95f0e29cda7925978e619b2f6
devAarno <devaarno@ya.ru>
2018-03-25 18:26:55 +0300

    Add lost errors file. #16

---
+++ src/net/socketError.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2017, 2018 Parkhomenko Stanislav
+ *
+ * This file is part of Lemon Server.
+ *
+ * Lemon Server is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SOCKETERROR_H
+#define SOCKETERROR_H
+
+typedef enum {
+    SE_OK, SE_SOCKET_ERROR, SE_LISTEN_ERROR, SE_BIND_ERROR, SE_ACCEPT_ERROR
+} socketError;
+
+#endif /* SOCKETERROR_H */
+
